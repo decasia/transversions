d481178e0e4f431dbd86e2f25f1b3ac4e6175c91
Eli Thorkelson <ethorkel@whittier.edu>
2016-03-04 09:20:21 -0800

    Bugfix: Don't make a definition atom when no text is selected

--- app/components/mobiledoc-definition-button/component.js
+++ app/components/mobiledoc-definition-button/component.js
@@ -7,7 +7,10 @@ export default Component.extend({
   // classNameBindings: ['isActive:active'],
   // isActive: computed.bool('editor.activeMarkupTagNames.isA'),
   click() {
-    let editor = this.get('editor.editor'); // editor is actually an instance of "postEditor", which contains an "editor"
+    let editor = this.get('editor.editor'); // 'editor' points to the ember editor component, which contains an instance of the actual mobiledoc editor class
+    if (!editor.range.head) { // only make a definition if text is selected
+      return;
+    }
     editor.run(postEditor => {
       const term = editor.cursor.selectedText(),
             mention = postEditor.builder.createAtom("definition-atom", term, {});
