04cb4236382dbf1c134e49fe7a1b2fc3dd3a0814
Eli Thorkelson <ethorkel@whittier.edu>
2016-03-21 12:24:21 -0700

    Testing: Add a sample atom to a transversion card.

--- server/mocks/docs.js
+++ server/mocks/docs.js
@@ -19,7 +19,13 @@ module.exports = function(app) {
         {
           source: {
             "version": "0.3.0",
-            "atoms": [],
+            "atoms": [
+              [
+                "definition-atom",
+                "prudence",
+                {id: 1, name: "prudence"}
+              ]
+            ],
             "cards": [],
             "markups": [],
             "sections": [
@@ -29,7 +35,25 @@ module.exports = function(app) {
                 [
                   [0, [], 0, "It is now some years since I detected how many were the false beliefs that I had from my earliest youth admitted as true, and how doubtful was everything I had since constructed on this basis; and from that time I was convinced that I must once for all seriously undertake to rid myself of all the opinions which I had formerly accepted, and commence to build anew from the foundation, if I wanted to establish any firm and permanent structure in the sciences.  But as this enterprise appeared to be a very great one, I waited until I had attained an age so mature that I could not hope that at any later date I should be better fitted to execute my design. This reason caused me to delay so long that I should feel that I was doing wrong were I to occupy in deliberation the time that yet remains to me for action.  To-day, then, since very opportunely for the plan I have in view I have delivered my mind from every care [and am happily agitated by no passions] and since I have procured for myself an assured leisure in a peaceable retirement, I shall at last seriously and freely address myself to the general upheaval of all my former opinions."]
                 ]
-              ]
+              ],
+              [
+                 1,
+                 "p",
+                 [
+                   [
+                     0,
+                     [],
+                     0,
+                     "Can we link to an author? "
+                   ],
+                   [
+                     1,
+                     [],
+                     0,
+                     0
+                   ]
+                 ]
+               ]
             ]
           },
           transversion: {
