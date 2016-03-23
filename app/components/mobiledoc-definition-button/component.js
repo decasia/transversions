60e47006965b1cbd859e3c1e5707a2cf0fea11ef
Eli Thorkelson <ethorkel@whittier.edu>
2016-03-23 12:16:08 -0700

    UI: Add definition component lets you pick which term to define, unless there is already an exact match in the glossary

--- app/components/mobiledoc-definition-button/component.js
+++ app/components/mobiledoc-definition-button/component.js
@@ -3,24 +3,70 @@ import Ember from 'ember';
 let { computed, Component, isPresent } = Ember;
 
 export default Component.extend({
-  tagName: 'button',
-  // classNameBindings: ['isActive:active'],
-  // isActive: computed.bool('editor.activeMarkupTagNames.isA'),
-  click() {
-    let editor = this.get('editor.editor'); // 'editor' points to the ember editor component, which contains an instance of the actual mobiledoc editor class
-    if (!editor.range.head) { // only make a definition if text is selected
+  store: Ember.inject.service(),
+  showPicker: false,
+  availableTerms: function() {
+     // should filter for this document's glossary
+    return this.get('store').findAll('term');
+  }.property(),
+  termSort: ['name'],
+  sortedTerms: computed.sort('availableTerms', 'termSort'),
+
+  addTermToSelection(term) {
+    const text = this.currentlySelectedText();
+    if (!isPresent(text)) { // don't add a definition if nothing's selected
       return;
     }
+
+    let editor = this.get('editor.editor'); // 'editor' points to the ember editor component, which contains an instance of the actual mobiledoc editor class
     editor.run(postEditor => {
-      const term = editor.cursor.selectedText();
-      if (!isPresent(term)) { // further sanity check for selected text
-        return;
-      }
-
-      const definition = postEditor.builder.createAtom("definition-atom", term, {id: 1, name: term});
-
+      const definition = postEditor.builder.createAtom("definition-atom", text, {
+        id: term.get('id'),
+        name: text
+      });
       postEditor.deleteRange(editor.range);
       postEditor.insertMarkers(editor.range.head, [definition]);
     });
+  },
+
+  currentlySelectedText() {
+    let editor = this.get('editor.editor');
+    if (!editor.range.head) {
+      return null;
+    }
+    return editor.cursor.selectedText();
+  },
+
+  actions: {
+    addDefinition() {
+      const text = this.currentlySelectedText();
+      if (!text) {
+        return;
+      }
+
+      this.get('store').findAll('term').then( (terms) => {
+        const existingTerm = terms.findBy('name', text);
+        if (existingTerm) {
+          this.addTermToSelection(existingTerm);
+        } else {
+          this.set('candidateText', text);
+          this.set('showPicker', true);
+        }
+      });
+    },
+    cancelPicker() {
+      this.set('showPicker', false);
+    },
+    pickTerm(existingTerm) {
+      this.addTermToSelection(existingTerm);
+      this.set('showPicker', false);
+    },
+    newTerm(name) {
+      const newTerm = this.get('store').createRecord('term', {name: name});
+      newTerm.save().then( () => {
+        this.addTermToSelection(newTerm);
+        this.set('showPicker', false);        
+      });
+    }
   }
 });
