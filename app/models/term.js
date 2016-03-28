d99a6c8fbd3126ac2dd51966c843ffb7bb796fde
Eli Thorkelson <ethorkel@whittier.edu>
2016-03-28 14:05:45 -0700

    Bugfix: New terms need to have default values for mobiledoc fields

--- app/models/term.js
+++ app/models/term.js
@@ -1,8 +1,16 @@
 import DS from 'ember-data';
 
+const emptyMobileDoc = {
+  version: "0.3.0",
+  markups: [],
+  atoms: [],
+  cards: [],
+  sections: []
+};
+
 export default DS.Model.extend({
   name: DS.attr('string'),
-  example: DS.attr('mobiledoc'),
-  definition: DS.attr('mobiledoc'),
-  discussion: DS.attr('mobiledoc')
+  example: DS.attr('mobiledoc', {defaultValue: emptyMobileDoc}),
+  definition: DS.attr('mobiledoc', {defaultValue: emptyMobileDoc}),
+  discussion: DS.attr('mobiledoc', {defaultValue: emptyMobileDoc})
 });
