cfa115bb7d5457baf06ad26bcff06fe6de421bd6
Eli Thorkelson <ethorkel@whittier.edu>
2016-03-28 16:14:42 -0700

    Bugfix: Default mobiledocs should be produced dynamically (or else they all refer to the same object)

--- app/models/term.js
+++ app/models/term.js
@@ -1,16 +1,18 @@
 import DS from 'ember-data';
 
-const emptyMobileDoc = {
-  version: "0.3.0",
-  markups: [],
-  atoms: [],
-  cards: [],
-  sections: []
-};
+function emptyMobileDoc() {
+  return {
+    version: "0.3.0",
+    markups: [],
+    atoms: [],
+    cards: [],
+    sections: []
+  };
+}
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  example: DS.attr('mobiledoc', {defaultValue: emptyMobileDoc}),
-  definition: DS.attr('mobiledoc', {defaultValue: emptyMobileDoc}),
-  discussion: DS.attr('mobiledoc', {defaultValue: emptyMobileDoc})
+  example: DS.attr('mobiledoc', {defaultValue: emptyMobileDoc()}),
+  definition: DS.attr('mobiledoc', {defaultValue: emptyMobileDoc()}),
+  discussion: DS.attr('mobiledoc', {defaultValue: emptyMobileDoc()})
 });
