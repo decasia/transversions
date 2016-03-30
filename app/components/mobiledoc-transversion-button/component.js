7404b990ba0adbfcccc267fa22851cede3623160
Eli Thorkelson <ethorkel@whittier.edu>
2016-03-30 11:47:31 -0700

    UI: Enable adding new transversion sections

---
+++ app/components/mobiledoc-transversion-button/component.js
@@ -0,0 +1,76 @@
+import Ember from 'ember';
+import Range from 'mobiledoc-kit/utils/cursor/range';
+
+let { Component } = Ember;
+
+function emptyMobileDoc() {
+  return {
+    version: "0.3.0",
+    markups: [],
+    atoms: [],
+    cards: [],
+    sections: []
+  };
+}
+
+function newTransversionCard(text) {
+  let source = emptyMobileDoc();
+  if (text) { // Only prepopulate text if there is any!
+    source.sections = [
+      [
+        1,
+        "p",
+        [[0,[],0,text]]
+      ]
+    ];
+  }
+  return {
+    summary: '',
+    transversion: emptyMobileDoc(),
+    source: source
+  };
+}
+
+export default Component.extend({
+  setupHotkey: function() {
+    let editor = this.get('editor.editor');
+    if (editor) {
+      editor.registerKeyCommand({
+        str: 'CTRL+T',
+        run: (editor) => this.addTransversion()
+      });
+    }
+  }.on('init'),
+
+  currentlySelectedText() {
+    let editor = this.get('editor.editor');
+    if (!editor.range.head) {
+      return null;
+    }
+    return editor.cursor.selectedText();
+  },
+
+  addTransversion() {
+    const editorComponent = this.get('editor'),
+          editor = editorComponent.editor,
+          section = editor.range.head.section;
+
+    if (section) {
+      // Remove text before adding it as a transversion!
+      editor.run(postEditor => {
+        editor.selectSections([section]);
+        const text = editor.cursor.selectedText(),
+              range = editor.range;
+
+        editorComponent.addCard('transversion-card', newTransversionCard(text));
+        postEditor.removeSection(section);
+      });
+    } else {
+      editorComponent.addCard('transversion-card', newTransversionCard(text));
+    }
+  },
+
+  click() {
+    this.addTransversion();
+  }
+});
--- app/doc/edit/controller.js
+++ app/doc/edit/controller.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  allowNewTransversions: true, // TODO: wire up to user acct
   actions: {
     saveChanges() {
       this.get('model').save(); //TODO: error handling
