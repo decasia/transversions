0aa2cbf26e558689033a6b1d5aaf570bf102a86d
Eli Thorkelson <ethorkel@whittier.edu>
2016-04-08 12:12:16 -0700

    UI: Improve docs index route

--- app/docs/route.js
+++ app/docs/route.js
@@ -1,20 +1,7 @@
 import Ember from 'ember';
-import blankMobileDoc from 'transverse/utils/blank-mobiledoc';
 
 export default Ember.Route.extend({
   model: function() {
-    return this.store.findAll('doc');
-  },
-  actions: {
-    newDocument() {
-      const name = prompt("Please enter the document name");
-      if (name) {
-        this.get('store').createRecord('doc', {
-          name: name,
-          content: blankMobileDoc()
-        }).save();
-      }
-
-    }
+    return this.store.findAll('work');
   }
 });
--- app/models/work.js
+++ app/models/work.js
@@ -6,5 +6,8 @@ export default DS.Model.extend({
   docs: hasMany('doc'),
   author: belongsTo('author'),
   name: attr('string'),
-  description: attr('string')
+  description: attr('string'),
+
+  docsSort: ['name'],
+  sortedDocs: Ember.computed.sort('docs', 'docsSort')
 });
