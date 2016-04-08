ce69cb91021ea82c047d582543272a1abc94606a
Eli Thorkelson <ethorkel@whittier.edu>
2016-04-08 12:32:30 -0700

    UI: Better organized index pages. Add breadcrumbs to child pages.

--- app/models/author.js
+++ app/models/author.js
@@ -1,9 +1,13 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 const {attr, hasMany} = DS;
 
 export default DS.Model.extend({
   works: hasMany('work'),
   name: attr('string'),
-  description: attr('string')
+  description: attr('string'),
+
+  workSort: ['name'],
+  sortedWorks: Ember.computed.sort('works', 'workSort')
 });
--- app/works/index/route.js
+++ app/works/index/route.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('work');
+    return this.store.findAll('work').then((works) => works.sortBy('name'));
   },
   setupController(controller, model) {
     controller.set('model', model);
