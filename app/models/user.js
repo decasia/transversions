d9616dde68eaf24aa8c5e97b63b1b1caf10b06f1
Eli Thorkelson <ethorkel@whittier.edu>
2016-11-14 14:11:08 -0800

    UI: Expose user's current group name in UI.

--- app/models/user.js
+++ app/models/user.js
@@ -4,5 +4,10 @@ import DS from 'ember-data';
 export default Model.extend({
   username: DS.attr('string'),
   email: DS.attr('string'),
-  groupData: DS.attr()
+  groupData: DS.attr(),
+  currentGroup: Ember.computed.filterBy('groupData', 'current', true),
+  currentGroupName: Ember.computed('currentGroup', function() {
+    let currentGroup = this.get('currentGroup');
+    return currentGroup ? currentGroup[0].name : null;
+  })
 });
